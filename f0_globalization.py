4d6ec06bac7f111eb538a4a419e75f322da43d81
Fernando Abreu <49952773+nandoabreu@users.noreply.github.com>
2019-12-17 15:39:24 -0300

    new file:   f0_globalization.py 	renamed:    1-read_repository.py -> f1_read_repository.py 	renamed:    2-protect_images.py -> f2_protect_images.py 	renamed:    3-remove_zip_images.py -> f3_remove_zip_images.py

---
+++ f0_globalization.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3
+
+npDir = "/Host/Temp/store_repository"
+repoDir = "/Host/Temp/store_repository/images"
+tmpDir = "/tmp"
+
+csvFile = "repositoryResults.csv"
+csvHeader = [ "IMAGE", "1st SOURCE", "FULL SIZE", "COMPRESSED SIZE", "USAGE", "APP", "SCREEN", "PRODUCT" ]
+
